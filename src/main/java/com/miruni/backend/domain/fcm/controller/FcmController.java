35be310a5d65a00fe476625570e2a3b4e6a020c8
oOccasio <dhzktldh@gmail.com>
2025-11-18 22:22:44 +0900

    refactor: #16 Swagger Api Add

--- src/main/java/com/miruni/backend/domain/fcm/controller/FcmController.java
+++ src/main/java/com/miruni/backend/domain/fcm/controller/FcmController.java
@@ -15,7 +15,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/fcm")
-public class FcmController {
+public class FcmController implements FcmApi {
 
     private final FcmTokenCommandService fcmTokenCommandService;
 
