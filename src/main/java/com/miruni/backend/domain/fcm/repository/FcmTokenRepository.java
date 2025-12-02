2635f7cf4500688125409330f92a9c2bf22a3660
oOccasio <dhzktldh@gmail.com>
2025-12-02 22:06:05 +0900

    feat(temp): #22 Notification Service

--- src/main/java/com/miruni/backend/domain/fcm/repository/FcmTokenRepository.java
+++ src/main/java/com/miruni/backend/domain/fcm/repository/FcmTokenRepository.java
@@ -3,6 +3,7 @@ package com.miruni.backend.domain.fcm.repository;
 import com.miruni.backend.domain.fcm.entity.FcmToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
@@ -10,4 +11,6 @@ public interface FcmTokenRepository extends JpaRepository<FcmToken, Long> {
     Optional<FcmToken> findByDeviceIdAndUserId(String deviceId, Long userId);
 
     boolean existsByDeviceId(String deviceId);
+
+    List<FcmToken> findByUserId(Long userId);
 }
---
+++ src/main/java/com/miruni/backend/domain/fcm/service/NotificationService.java
@@ -0,0 +1,244 @@
+package com.miruni.backend.domain.fcm.service;
+
+import com.google.firebase.messaging.FirebaseMessaging;
+import com.google.firebase.messaging.FirebaseMessagingException;
+import com.google.firebase.messaging.Message;
+import com.google.firebase.messaging.Notification;
+import com.miruni.backend.domain.fcm.entity.FcmToken;
+import com.miruni.backend.domain.fcm.exception.FcmErrorCode;
+import com.miruni.backend.domain.plan.entity.AiPlan;
+import com.miruni.backend.domain.plan.entity.BasicPlan;
+import com.miruni.backend.global.exception.BaseException;
+import lombok.Builder;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ScheduledFuture;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class NotificationService {
+
+    private final TaskScheduler taskScheduler;
+
+    // 스케줄된 작업들을 추적하는 맵
+    private final Map<String, ScheduledFuture<?>> scheduledTasks = new ConcurrentHashMap<>();
+    private final FcmTokenQueryService fcmTokenQueryService;
+    private final FirebaseMessaging firebaseMessaging;
+
+    //Plan 알림 등록
+    public void scheduleNotification(BasicPlan plan){
+
+        LocalDateTime scheduledTime = validateAndGetScheduledTime(
+                plan.isDone(),
+                plan.getId(),
+                LocalDateTime.of(plan.getScheduledDate(), plan.getScheduledTime())
+        );
+
+        if (scheduledTime == null) return;
+
+        scheduleAllNotifications(
+                NotificationTask
+                        .builder()
+                        .userId(plan.getUser().getId())
+                        .targetId(plan.getId())
+                        .type(PlanType.PLAN)
+                        .scheduledTime(scheduledTime)
+                        .taskTitle(plan.getTitle())
+                        .build()
+        );
+
+
+        log.info("Plan 알림 스케줄 등록 완료: planId = {}, startTime = {}", plan.getId(), plan.getScheduledTime());
+    }
+
+    //AIPlan 알림 등록
+    public void scheduleNotification(AiPlan aiplan){
+
+        LocalDateTime scheduledTime = validateAndGetScheduledTime(
+                aiplan.isDone(),
+                aiplan.getId(),
+                LocalDateTime.of(aiplan.getScheduledDate(), aiplan.getScheduledTime())
+        );
+
+        if (scheduledTime == null) return;
+
+
+        scheduleAllNotifications(
+                NotificationTask
+                        .builder()
+                        .userId(aiplan.getPlan().getUser().getId())
+                        .targetId(aiplan.getId())
+                        .type(PlanType.AI_PLAN)
+                        .scheduledTime(scheduledTime)
+                        .taskTitle(aiplan.getSubTitle())
+                        .build()
+        );
+
+        log.info("AiPlan 알림 스케줄 등록 완료: planId = {}, startTime = {}", aiplan.getId(), aiplan.getScheduledTime());
+    }
+
+
+    //활성화 토큰 여부 확인
+    private void scheduleAllNotifications(NotificationTask task) {
+        List<FcmToken> fcmTokens = fcmTokenQueryService.getTokensByUserId(task.userId());
+
+        for (FcmToken fcmToken : fcmTokens) {
+            if (fcmToken.isBefore5minAlarm()) {
+               scheduleAlarm(task, AlarmType.BEFORE_5MIN, fcmToken.getToken());
+
+            }
+            if (fcmToken.isBefore10minAlarm()) {
+               scheduleAlarm(task, AlarmType.BEFORE_10MIN, fcmToken.getToken());
+            }
+            if (fcmToken.isPopupAlarm()) {
+                scheduleAlarm(task, AlarmType.POPUP, fcmToken.getToken());
+
+            }
+            if (fcmToken.isNagAlarm()) {
+                scheduleAlarm(task, AlarmType.NAG, fcmToken.getToken());
+            }
+        }
+    }
+
+
+    //스케줄러에 알람 등록
+    private void scheduleAlarm(NotificationTask task, AlarmType alarmType, String token) {
+        LocalDateTime notificationTime = calculateTime(task.scheduledTime, alarmType);
+
+        String scheduleKey = createScheduleKey(task.type(), task.targetId(), alarmType, token);
+
+
+        ScheduledFuture<?> future = taskScheduler.schedule(
+                () -> executeNotification(task, alarmType, token),
+                notificationTime.atZone(ZoneId.systemDefault()).toInstant()
+        );
+
+        scheduledTasks.put(scheduleKey, future);
+    }
+
+    //알람 실행 메서드
+    private void executeNotification(NotificationTask task, AlarmType alarmType, String token) {
+        try {
+            // 팝업/잔소리는 시작 여부 확인
+            if (isTaskAlreadyStarted(task))
+                return;
+
+            sendNotification(task, alarmType, token);
+
+        } catch (Exception e) {
+            log.error("알림 전송 실패", e);
+        } finally {
+            String scheduleKey = createScheduleKey(task.type(), task.targetId(), alarmType, token);
+            scheduledTasks.remove(scheduleKey);
+        }
+    }
+
+    //시간계산
+    private LocalDateTime calculateTime(LocalDateTime scheduledTime, AlarmType alarmType) {
+        return switch (alarmType) {
+            case BEFORE_5MIN -> scheduledTime.minusMinutes(5);
+            case BEFORE_10MIN -> scheduledTime.minusMinutes(10);
+            case POPUP -> scheduledTime;
+            case NAG -> scheduledTime.plusMinutes(5);
+        };
+    }
+
+
+    //제목 및 문구 만들기
+    private NotificationContent createContent(String title, AlarmType alarmType) {
+        return switch (alarmType) {
+            case BEFORE_5MIN -> new NotificationContent(
+                    String.format("5분 뒤에 '%s'가 예정되어 있어요!", title),
+                    String.format("곧 '%s' 시간이에요!", title)
+            );
+            case BEFORE_10MIN -> new NotificationContent(
+                    String.format("10분 뒤에 '%s'가 예정되어 있어요!", title),
+                    String.format("10분 후 '%s' 일정을 시작하고, 땅콩 3개를 획득하세요!", title)
+            );
+            case POPUP -> new NotificationContent(
+                    String.format("'%s'를 시작하세요!", title),
+                    String.format("지금 바로 '%s' 일정을 시작하세요!", title)
+            );
+            case NAG -> new NotificationContent(
+                    String.format("'%s'를 얼른 시작하세요!", title),
+                    String.format("지금 바로 '%s' 일정을 시작하지 않으면, 땅콩을 잃어버릴거에요", title)
+            );
+        };
+    }
+
+    //스케줄키 생성
+    private String createScheduleKey(PlanType type, Long targetId, AlarmType alarmType, String token) {
+        return String.format("%s_%d_%s_%s", type.name(), targetId, alarmType.name(), token);
+    }
+
+    //실제 알람 전송
+    private void sendNotification(NotificationTask task, AlarmType alarmType, String token) {
+        NotificationContent content = createContent(task.taskTitle(), alarmType);
+
+        try {
+            Message message = Message.builder()
+                    .putData("type", task.type().name())
+                    .putData("targetId", task.targetId().toString())
+                    .putData("alarmType", alarmType.name())
+                    .setNotification(Notification.builder()
+                            .setTitle(content.title)
+                            .setBody(content.body)
+                            .build())
+                    .setToken(token)
+                    .build();
+
+            firebaseMessaging.send(message);
+
+        }catch (FirebaseMessagingException e){
+        throw BaseException.type(FcmErrorCode.FCM_SEND_FAILED);
+        }
+    }
+
+    private LocalDateTime validateAndGetScheduledTime(boolean isDone, Long planId, LocalDateTime scheduledTime) {
+        if (isDone) {
+            throw BaseException.type(FcmErrorCode.ALREADY_FINISHED_TASK);
+        }
+
+        if (scheduledTime.isBefore(LocalDateTime.now())) {
+            log.warn("이미 지난 일정: planId = {}, scheduledTime = {}", planId, scheduledTime);
+            return null;
+        }
+
+        return scheduledTime;
+    }
+
+    // == 관련 record == //
+    @Builder
+    private record NotificationTask(
+            Long userId,
+            Long targetId,
+            PlanType type,  // PLAN or AI_PLAN
+            LocalDateTime scheduledTime,
+            String taskTitle  // Plan의 title이나 AiPlan의 description
+    ) {}
+
+    private enum PlanType {
+        PLAN, AI_PLAN
+    }
+
+    private enum AlarmType {
+        BEFORE_5MIN,
+        BEFORE_10MIN,
+        POPUP,
+        NAG
+    }
+
+    private record NotificationContent(
+            String title,
+            String body
+    ){}
+}
