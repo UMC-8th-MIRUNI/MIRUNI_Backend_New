c2bd3093aaddfce097adf6bff6ba7c30d7408152
chanmi05 <chanmi05244198@gmail.com>
2026-01-14 20:31:53 +0900

    [refactor] #27 AiPlanRepository,PlanQueryService, PlanController  주석처리

--- src/main/java/com/miruni/backend/domain/plan/controller/PlanController.java
+++ src/main/java/com/miruni/backend/domain/plan/controller/PlanController.java
@@ -29,20 +29,20 @@ public class PlanController implements PlanApi{
     private final PlanCommandService planCommandService;
     private final PlanQueryService planQueryService;
 
-    @GetMapping("/home")
-    public PlanHomeResponse getPlanHome(@LoginUser Long userId) {
-        return planQueryService.getPlanHome(userId);
-    }
-
-    @GetMapping("/monthly")
-    public List<MonthlyPlanResponse> getMonthlyPlans(@LoginUser Long userId, @RequestParam int year, @RequestParam int month) {
-        return planQueryService.getMonthlyPlan(userId, year, month);
-    }
-
-    @GetMapping("/daily")
-    public DailyPlanResponse getDailyPlans(@LoginUser Long userId, @RequestParam int year, @RequestParam int month, @RequestParam int day) {
-        return planQueryService.getDailyPlan(userId, year, month, day);
-    }
+//    @GetMapping("/home")
+//    public PlanHomeResponse getPlanHome(@LoginUser Long userId) {
+//        return planQueryService.getPlanHome(userId);
+//    }
+//
+//    @GetMapping("/monthly")
+//    public List<MonthlyPlanResponse> getMonthlyPlans(@LoginUser Long userId, @RequestParam int year, @RequestParam int month) {
+//        return planQueryService.getMonthlyPlan(userId, year, month);
+//    }
+//
+//    @GetMapping("/daily")
+//    public DailyPlanResponse getDailyPlans(@LoginUser Long userId, @RequestParam int year, @RequestParam int month, @RequestParam int day) {
+//        return planQueryService.getDailyPlan(userId, year, month, day);
+//    }
 
     @GetMapping("/{planId}")
     public PlanDetailResponse getPlanDetail(@LoginUser Long userId, @PathVariable Long planId, @RequestParam PlanType planType) {
